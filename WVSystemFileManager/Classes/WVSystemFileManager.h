e00a67392d795ab2ccf1034700d756484e0c7a06
wenva <wenva@xuwenfadeMacBook-Pro.local>
2014-03-19 19:25:39 +0800

    init

---
+++ WVSystemFileManager/Classes/WVSystemFileManager.h
@@ -0,0 +1,13 @@
+//
+//  WVSystemFileManager.h
+//  WVSystemFileManager
+//
+//  Created by wenva on 3/19/14.
+//  Copyright (c) 2014 wenva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WVSystemFileManager : NSObject
+
+@end
---
+++ WVSystemFileManager/Classes/WVSystemFileManager.m
@@ -0,0 +1,13 @@
+//
+//  WVSystemFileManager.m
+//  WVSystemFileManager
+//
+//  Created by wenva on 3/19/14.
+//  Copyright (c) 2014 wenva. All rights reserved.
+//
+
+#import "WVSystemFileManager.h"
+
+@implementation WVSystemFileManager
+
+@end
